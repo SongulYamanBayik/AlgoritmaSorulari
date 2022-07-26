881c68423b7af4f46c43483c4fd0f85956cd3156
Hayri BAYIK <hyr.byk@gmail.com>
2022-07-26 12:44:22 +0300

    V1: Basit kare alma ve toplama işlemi

---
+++ KarelerinToplami/KarelerinToplami/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KarelerinToplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("ilk sayıyı giriniz");
+                int sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ikinci sayıyı giriniz");
+                int sayi2 = Convert.ToInt32(Console.ReadLine());
+                int toplam = (sayi1 * sayi1) + (sayi2 * sayi2);
+                Console.WriteLine("iki sayının karelerinin toplamı: " + toplam);
+            }
+            catch 
+            {
+
+                Console.WriteLine("Numerik bir değer girmediniz!");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
