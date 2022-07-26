40a78462201836d706bc681697882db151dde83e
Songül YAMAN BAYIK <snglymn@gmail.com>
2022-07-26 13:30:36 +0300

    math.pow kullanarak iki sayının karelerinin toplamını bulma

--- KarelerinToplami/KarelerinToplami/Program.cs
+++ KarelerinToplami/KarelerinToplami/Program.cs
@@ -6,21 +6,24 @@ namespace KarelerinToplami
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("ilk sayıyı giriniz");
-                int sayi1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("ikinci sayıyı giriniz");
-                int sayi2 = Convert.ToInt32(Console.ReadLine());
-                int toplam = (sayi1 * sayi1) + (sayi2 * sayi2);
-                Console.WriteLine("iki sayının karelerinin toplamı: " + toplam);
-            }
-            catch 
-            {
+            //try
+            //{
+            //    Console.WriteLine("ilk sayıyı giriniz");
+            //    int sayi1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("ikinci sayıyı giriniz");
+            //    int sayi2 = Convert.ToInt32(Console.ReadLine());
+            //    int toplam = (sayi1 * sayi1) + (sayi2 * sayi2);
+            //    Console.WriteLine("iki sayının karelerinin toplamı: " + toplam);
+            //}
+            //catch 
+            //{
 
-                Console.WriteLine("Numerik bir değer girmediniz!");
-            }
-            
+            //    Console.WriteLine("Numerik bir değer girmediniz!");
+            //}
+
+            Console.WriteLine("Birinci ve ikinci sayıları giriniz");
+           double toplam =Math.Pow(Convert.ToInt32(Console.ReadLine()),2) + Math.Pow( Convert.ToInt32(Console.ReadLine()),2);
+            Console.WriteLine("iki sayının karelerinin toplamı: "+toplam);
 
             Console.ReadKey();
         }
